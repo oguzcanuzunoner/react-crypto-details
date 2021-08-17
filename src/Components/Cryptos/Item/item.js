5da076013bf13a9e5115ea4340d6517b511449ee
imsamet <imsametcetin@gmail.com>
2021-08-17 19:44:35 +0300

    Table click update

--- src/Components/Cryptos/Item/item.js
+++ src/Components/Cryptos/Item/item.js
@@ -1,35 +1,30 @@
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Style from "./item.module.css";
-import * as ReactBootStrap from "react-bootstrap";
 
-function Item({val}) {
-    return(
-      <Link
-        to={val.id}
-        className={Style.link}
-      >
-        <tr className={Style.item}>
-          <td>
-            <img
-              src={val.image}
-              alt={val.symbol.toUpperCase()}
-            />
-          </td>
-          <td>{val.name}</td>
-          {/* <td>{val.symbol.toUpperCase()}</td> */}
-          <td>{val.current_price}</td>
-          {val.market_cap_change_percentage_24h < 0 ? (
-            <td style={{ color: "#C62A88" }}>
-              {val.market_cap_change_percentage_24h.toFixed(2)}
-            </td>
-          ) : (
-            <td style={{ color: "#03C4A1" }}>
-              {val.market_cap_change_percentage_24h.toFixed(2)}
-            </td>
-          )}
-        </tr>
-      </Link>
-    )
+function Item({ val }) {
+  const history = useHistory();
+  const handleRowClick = (row) => {
+    history.push(`/${row.id}`);
+    console.log(row);
+  };
+  return (
+    <tr onClick={() => handleRowClick(val)} className={Style.item}>
+      <td>
+        <img src={val.image} alt={val.symbol.toUpperCase()} />
+      </td>
+      <td>{val.name}</td>
+      <td>{val.current_price}</td>
+      {val.market_cap_change_percentage_24h < 0 ? (
+        <td style={{ color: "#C62A88" }}>
+          {val.market_cap_change_percentage_24h.toFixed(2)}
+        </td>
+      ) : (
+        <td style={{ color: "#03C4A1" }}>
+          {val.market_cap_change_percentage_24h.toFixed(2)}
+        </td>
+      )}
+    </tr>
+  );
 }
 
-export default Item
+export default Item;
